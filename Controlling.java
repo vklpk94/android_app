d59a6a9f51db2ab8ed76f9913f11369f71d7fe76
Khang Nguyen <kenz.nguyen.94@gmail.com>
2020-12-24 16:17:33 -0800

    Create Controlling.java

---
+++ Controlling.java
@@ -0,0 +1,277 @@
+package com.example.ardconpad;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.ardconpad.ActivityHelper;
+import com.example.ardconpad.MainActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class Controlling extends AppCompatActivity {
+   private static final String TAG = "BlueTest5-Controlling";
+   private int mMaxChars = 50000;//Default//change this to string..........
+   private UUID mDeviceUUID;
+   private BluetoothSocket mBTSocket;
+   private ReadInput mReadThread = null;
+
+   private boolean mIsUserInitiatedDisconnect = false;
+   private boolean mIsBluetoothConnected = false;
+
+   private Button mBtnDisconnect;
+   private BluetoothDevice mDevice;
+
+   final static String swt1="92";
+   final static String swt2="81";
+   final static String swt3="79";
+   final static String left_button="68";
+   final static String right_button="59";
+
+   private ProgressDialog progressDialog;
+   Button m_1, m_2, m_3, left_btn, right_btn;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_controlling);
+
+       ActivityHelper.initialize(this);
+       // mBtnDisconnect = (Button) findViewById(R.id.btnDisconnect);
+       m_1=(Button)findViewById(R.id.swt1);
+       m_2=(Button)findViewById(R.id.swt2);
+       m_3=(Button)findViewById(R.id.swt3);
+       left_btn=(Button)findViewById(R.id.left_button);
+       right_btn=(Button)findViewById(R.id.right_button);
+
+       Intent intent = getIntent();
+       Bundle b = intent.getExtras();
+       mDevice = b.getParcelable(MainActivity.DEVICE_EXTRA);
+       mDeviceUUID = UUID.fromString(b.getString(MainActivity.DEVICE_UUID));
+       mMaxChars = b.getInt(MainActivity.BUFFER_SIZE);
+
+       Log.d(TAG, "Ready");
+
+       m_1.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v) {
+               try {
+                   mBTSocket.getOutputStream().write(swt1.getBytes());
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }});
+       m_2.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v) {
+               try {
+                   mBTSocket.getOutputStream().write(swt2.getBytes());
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }});
+       m_3.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v) {
+               try {
+                   mBTSocket.getOutputStream().write(swt3.getBytes());
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }});
+       left_btn.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v) {
+               try {
+                   mBTSocket.getOutputStream().write(left_button.getBytes());
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }});
+       right_btn.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v) {
+               try {
+                   mBTSocket.getOutputStream().write(right_button.getBytes());
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }});
+   }
+
+   private class ReadInput implements Runnable {
+
+       private boolean bStop = false;
+       private Thread t;
+
+       public ReadInput() {
+           t = new Thread(this, "Input Thread");
+           t.start();
+       }
+
+       public boolean isRunning() {
+           return t.isAlive();
+       }
+
+       @Override
+       public void run() {
+           InputStream inputStream;
+           try {
+               inputStream = mBTSocket.getInputStream();
+               while (!bStop) {
+                   byte[] buffer = new byte[256];
+                   if (inputStream.available() > 0) {
+                       inputStream.read(buffer);
+                       int i = 0;
+                       for (i = 0; i < buffer.length && buffer[i] != 0; i++) {
+                       }
+                       final String strInput = new String(buffer, 0, i);
+                   }
+                   Thread.sleep(500);
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+
+       public void stop() {
+           bStop = true;
+       }
+   }
+
+   private class DisConnectBT extends AsyncTask<Void, Void, Void> {
+
+       @Override
+       protected void onPreExecute() {
+       }
+
+       @Override
+       protected Void doInBackground(Void... params) {
+           if (mReadThread != null) {
+               mReadThread.stop();
+               while (mReadThread.isRunning())
+                   ; // Wait until it stops
+               mReadThread = null;
+           }
+
+           try {
+               mBTSocket.close();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+           return null;
+       }
+
+       @Override
+       protected void onPostExecute(Void result) {
+           super.onPostExecute(result);
+           mIsBluetoothConnected = false;
+           if (mIsUserInitiatedDisconnect) {
+               finish();
+           }
+       }
+   }
+
+   private void msg(String s) {
+       Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+   }
+
+   @Override
+   protected void onPause() {
+       if (mBTSocket != null && mIsBluetoothConnected) {
+           new DisConnectBT().execute();
+       }
+       Log.d(TAG, "Paused");
+       super.onPause();
+   }
+
+   @Override
+   protected void onResume() {
+       if (mBTSocket == null || !mIsBluetoothConnected) {
+           new ConnectBT().execute();
+       }
+       Log.d(TAG, "Resumed");
+       super.onResume();
+   }
+
+   @Override
+   protected void onStop() {
+       Log.d(TAG, "Stopped");
+       super.onStop();
+   }
+
+   @Override
+   protected void onSaveInstanceState(Bundle outState) {
+       super.onSaveInstanceState(outState);
+   }
+
+   private class ConnectBT extends AsyncTask<Void, Void, Void> {
+       private boolean mConnectSuccessful = true;
+
+       @Override
+       protected void onPreExecute() {
+
+           progressDialog = ProgressDialog.show(Controlling.this, "Hold on", "Connecting");// http://stackoverflow.com/a/11130220/1287554
+
+       }
+
+       @Override
+       protected Void doInBackground(Void... devices) {
+
+           try {
+               if (mBTSocket == null || !mIsBluetoothConnected) {
+                   mBTSocket = mDevice.createInsecureRfcommSocketToServiceRecord(mDeviceUUID);
+                   BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                   mBTSocket.connect();
+               }
+           } catch (IOException e) {
+               mConnectSuccessful = false;
+           }
+           return null;
+       }
+
+       @Override
+       protected void onPostExecute(Void result) {
+           super.onPostExecute(result);
+           if (!mConnectSuccessful) {
+               Toast.makeText(getApplicationContext(), "Could not connect to device.Please turn on your Hardware", Toast.LENGTH_LONG).show();
+               finish();
+           } else {
+               msg("Connected to device");
+               mIsBluetoothConnected = true;
+               mReadThread = new ReadInput(); // Kick off input reader
+           }
+
+           progressDialog.dismiss();
+       }
+   }
+   @Override
+   protected void onDestroy() {
+       super.onDestroy();
+   }
+}
+
